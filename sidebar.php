fb431cd321daf88bcbe3a2eafd0cb2e808fc03fc
Megaju <julien_malle@outlook.fr>
2016-08-31 12:19:26 +0200

    menu navigation top ok

--- sidebar.php
+++ sidebar.php
@@ -7,8 +7,6 @@
         <li id="search"><?php include(TEMPLATEPATH . '/searchform.php'); ?></li>
         <!-- categories -->
         <li><h2>Categories</h2><ul><?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?></ul></li>
-        <!-- pages -->
-        <?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
         <!-- calendar -->
         <li id="calendar"><h2>Calendrier</h2><?php get_calendar(); ?></li>
     </ul> 
