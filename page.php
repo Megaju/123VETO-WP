4efd4fca8c6f1af68d5250b9e121841289dfde4b
Becca85 <rebeccamaccio@gmail.com>
2016-09-02 15:19:15 +0200

    mergeliZZZZZe

---
+++ page.php
@@ -0,0 +1,23 @@
+<?php //fix bug header ?>
+<?php
+/*
+Template Name: pagetype
+*/
+?>
+
+<!-- insertion du header -->
+<?php get_header(); ?>
+<!-- insertion de la sidebar -->
+<?php get_sidebar(); ?>
+
+
+
+
+
+
+
+
+
+
+<!-- insertion du footer -->
+<?php get_footer(); ?>
