bfd2e9beb0c927cf0e1cf2c39941b34f417e759c
Megaju <julien_malle@outlook.fr>
2016-09-02 16:56:43 +0200

    prise rdv en template

--- index.php
+++ index.php
@@ -12,18 +12,23 @@
                 
                  <div class="flexbox">                  
                     <div class="docteur" id="docteur1">
-                       <div class="docteur-present">
+                       <div class="docteur-present flexwrap">
+                          <h3>Dr Aurélie Mayoussier</h3>
+                          <h4>Diplômée de l’école vétérinaire de MaisonsAlfort en 2008</h4>
                            <p>Dr Aurélie Mayoussier Diplômée de l’école vétérinaire de MaisonsAlfort en 2008, elle se passionne pour la médecine de l’élevage et de la reproduction et la médecine des NAC.
 
                             Le Dr Mayoussier possède deux chats Pratchett et Bubule ainsi que Bambou, chien loup de Saarloos.</p>
                        </div>  
                     </div>
                     <div class="docteur" id="docteur2">
-                       <div class="docteur-present">
+                       <div class="docteur-present flexwrap">
+                       <h3>Dr Eric Wintz</h3>
+                          <h4>Diplômé de l’école vétérinaire de Nantes en 1990</h4>
                         <p>Dr Eric Wintz Diplômé de l’école vétérinaire de Nantes en 1990, il se passionne pour la dermatologie et la cardiologie. Le Dr Wintz possède quatre chats Leelas (européenne), Gribouille (européen), Hannah (Mane Coon), et Joey (Ragdoll), des Platis, deux lapins nains Neige et Teddy ainsi que Lexie, chihuahua.</p>
                         </div>
                     </div>
                 </div>
+                
             </div>
             
             <div id="bloc-savoirfaire">
