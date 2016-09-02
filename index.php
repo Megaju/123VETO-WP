1776a34bd92111f6d2d3637eca507f34987060fe
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-02 11:39:59 +0200

    debut fondation index

--- index.php
+++ index.php
@@ -1,22 +1,87 @@
-    <?php get_header(); ?>
+<?php get_header(); ?>
 
     <div id="page">
-       
+
         <!-- contenue du site -->
         <div id="content">
-            
-            
-            
+
+
+
         </div>
-        
-    <!-- aside -->    	
-    <?php get_sidebar(); ?>
-       
-    
+        <section id="attente-bootstrap">
+        <!-- aside -->
+        <?php get_sidebar(); ?>
+        <div>
+            <section id="formeindex">
+                <h2>Notre équipe à votre service</h2>
+                <div class="index-separator"></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aspernatur dolorum ullam quisquam ut qui velit molestias inventore esse asperiores modi possimus.</p>
+
+                <div class="equipe">
+                  
+                    <div class="docteur">
+                        <p>Houhou, ici img docteur + nom et faut faire un beau hover, t'es content Ju' =D</p>
+                    </div>
+
+                    <div class="docteur">
+                        <p>Houhou, ici img docteur + nom et faut faire un beau hover, t'es content Ju' =D</p>
+                    </div>
+                </div>
+            </section>
+            
+            <section id="formeindex">
+                <h2>Notre savoir-faire</h2>
+                <div class="index-separator"></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aspernatur dolorum ullam quisquam ut qui velit molestias inventore esse asperiores modi possimus.</p>
+
+                <div class="savoirfaire">
+                  
+                    <div class="categorie">
+                        <div class="simulation-icone"></div>
+                        <p>Eclate toi avec les fonts icones =D</p>
+                    </div>
+
+                    <div class="categorie">
+                       <div class="simulation-icone"></div>
+                        <p>Eclate toi avec les fonts icones =D</p>
+                    </div>
+                    
+                    <div class="categorie">
+                       <div class="simulation-icone"></div>
+                        <p>Eclate toi avec les fonts icones =D</p>
+                    </div>
+                    
+                    <div class="categorie">
+                       <div class="simulation-icone"></div>
+                        <p>Eclate toi avec les fonts icones =D</p>
+                    </div>
+                </div>
+            </section>
+            
+            <section id="equipe">
+                <h2>Notre savoir-faire</h2>
+                <div class="index-separator"></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aspernatur dolorum ullam quisquam ut qui velit molestias inventore esse asperiores modi possimus.</p>
+
+                <div class="equipe">
+                  
+                    <div class="docteur">
+                        <p>Houhou, ici img docteur + nom et faut faire un beau hover, t'es content Ju' =D</p>
+                    </div>
+
+                    <div class="docteur">
+                        <p>Houhou, ici img docteur + nom et faut faire un beau hover, t'es content Ju' =D</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+            
+        </section>
+
     </div>
-    
+
     <?php get_footer(); ?>
 
-</body>
+        </body>
 
-</html>
+        </html>
