1441c650dd90d90c7ff4c35f48c4e0f1c9620114
solenehuault <solene.huault@gmail.com>
2016-09-02 16:14:37 +0200

    link jquery v3

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
         <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
         <link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php bloginfo('atom_url'); ?>" />
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+				<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery_v3.1.0.js"></script>           
 				<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/button_fixed.js"></script>           
             <?php wp_get_archives('type=monthly&format=link'); ?>
                 
