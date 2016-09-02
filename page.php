e24308d90f51ef8311866178643227daf6e74ff8
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-02 16:41:58 +0200

    Ajout font + bouton

--- page.php
+++ page.php
@@ -10,7 +10,35 @@ Template Name: pagetype
 <!-- pour insérer la sidebar -->
 <?php get_sidebar(); ?>
 
-<i class="fa fa-camera-retro fa-5x" style="color:blue"></i>
+<div>
+   <i class="fa fa-stethoscope fa-4x"></i>
+    <button>Consul.</button>
+</div>
+
+<div>
+    <i class="fa fa-camera fa-4x"></i>
+    <button>Imagerie</button>
+</div>
+
+<div>
+   <i class="fa fa-medkit fa-4x text-center"></i>
+    <button>Analyses</button>
+</div>
+
+<div>
+   <i class="fa fa-heartbeat fa-4x"></i>
+    <button>Chirurgie</button>
+</div>
+
+<div>
+   <i class="fa fa-hospital-o fa-4x"></i>
+    <button>Hosp.</button>
+</div>
+
+<div>
+   <i class="fa fa-paw fa-4x"></i>
+    <button>NAC</button>
+</div>
 
 
 
