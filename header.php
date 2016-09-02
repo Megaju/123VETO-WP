99ea3eb36330bbcf831c806007c2ccac0326a70d
solenehuault <solene.huault@gmail.com>
2016-09-02 11:00:22 +0200

    header - ajout texte descriptif

--- header.php
+++ header.php
@@ -43,9 +43,10 @@
                 <div class="logo-img img-responsive"></div>
                 <!-- infos -->
                 <div class="info">
-                    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+                    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> - <?php bloginfo('description'); ?></h1>
                     <h2>Dr Eric Wintz - Dr Aurélie Mayoussier</h2>
-                    <p class="description"><?php bloginfo('description'); ?></p>
+                    <p class="description">123 Véto vous accueille du lundi au vendredi de 9h à 12h et de 14h à 19h et le samedi de 9h à 12h</p>
+                    <p class="description">Les urgences sont assurées 24h/24 7 jours sur 7 sur appel téléphonique au 02 40 01 61 69</p>
                 </div>
             </div>
         </div>
