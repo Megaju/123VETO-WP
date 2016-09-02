8f9b4dd1ec59b549ac2f792b53e73ac5ea37e205
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-02 17:17:21 +0200

    Mise en place bouton

--- page.php
+++ page.php
@@ -5,39 +5,54 @@
 Template Name: pagetype
 */
 ?>
+
+
 <!-- pour insérer le header -->
 <?php get_header(); ?>
+
+<div id="attente-bootstrap">
 <!-- pour insérer la sidebar -->
 <?php get_sidebar(); ?>
 
 <div>
-   <i class="fa fa-stethoscope fa-4x"></i>
-    <button>Consul.</button>
+<section id="formeindex">
+   <h2>Notre savoir-faire</h2>
+                <div class="index-separator"></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aspernatur dolorum ullam quisquam ut qui velit molestias inventore esse asperiores modi possimus.</p>
+    <div class="icone-generique">
+    <div class="center-generique">
+       <i class="fa fa-stethoscope fa-4x"></i>
+        <button class="bouton-generique" onclick="affich_consultation()">Consul.</button>
+    </div>
+
+    <div class="center-generique">
+        <i class="fa fa-camera fa-4x"></i>
+        <button class="bouton-generique" onclick="affich_imagerie()">Imagerie</button>
+    </div>
+
+    <div class="center-generique">
+       <i class="fa fa-medkit fa-4x text-center"></i>
+        <button class="bouton-generique" onclick="affich_analyse()" >Analyses</button>
+    </div>
+
+    <div class="center-generique">
+       <i class="fa fa-heartbeat fa-4x"></i>
+        <button class="bouton-generique" onclick="affich_chirurgie()">Chirurgie</button>
+    </div>
+
+    <div class="center-generique">
+       <i class="fa fa-hospital-o fa-4x"></i>
+        <button class="bouton-generique" onclick="affich_hospitalisation()">Hosp.</button>
+    </div>
+
+    <div class="center-generique">
+       <i class="fa fa-paw fa-4x"></i>
+        <button class="bouton-generique" onclick="affich_nac()">NAC</button>
+    </div>
+    </div>
+</section>
 </div>
 
-<div>
-    <i class="fa fa-camera fa-4x"></i>
-    <button>Imagerie</button>
-</div>
-
-<div>
-   <i class="fa fa-medkit fa-4x text-center"></i>
-    <button>Analyses</button>
-</div>
-
-<div>
-   <i class="fa fa-heartbeat fa-4x"></i>
-    <button>Chirurgie</button>
-</div>
-
-<div>
-   <i class="fa fa-hospital-o fa-4x"></i>
-    <button>Hosp.</button>
-</div>
-
-<div>
-   <i class="fa fa-paw fa-4x"></i>
-    <button>NAC</button>
 </div>
 
 
