7d8f71ef2b255fb7b2e790722cf13f25560853ce
solenehuault <solene.huault@gmail.com>
2016-09-07 12:03:16 +0200

    ajout des noms de notre équipe

--- footer.php
+++ footer.php
@@ -7,6 +7,6 @@
         <p> Copyright &#169;
             <?php print(date(Y)); ?>
                 <?php bloginfo('name'); ?>
-                    <br /> Site propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="#">Julien, Lise, Solène</a>
+                    <br /> Site propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="#">Julien, Lise, Solène, Valentin, Floriant, Jean-Patrice, Stéphane, Gael, Rebecca</a>
         </p>
     </div>
