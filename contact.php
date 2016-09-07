a4125761ca97feab1d93a1f86ff16a85ce680c8a
FloryanL <floryan.lollivier@gmail.com>
2016-09-07 11:22:36 +0200

    modifs flexbox

--- contact.php
+++ contact.php
@@ -7,7 +7,45 @@
 <?php get_header(); ?>
 
 <!--Formulaire de contact-->
-	<h2 class="TitreForm">NOUS CONTACTER PAR MAIL</h2>
+	<div class="contact">
+		<h2 class="TitreForm">NOUS CONTACTER PAR MAIL</h2>
+		
+		<div class="formulaire">
+			<form action="formulaire de contact" method="post">
+					
+					<input type="text" name="nom" placeholder="NOM" class="identite"></input> 
+					</br></br>
+					
+					<input type="text" name="Prenom" placeholder="Prenom"></input>
+					</br></br>
+					
+					<input type="text" name="Animal" placeholder="Nom de l'animal"></input>
+					</br></br>
+
+
+					<select name="Type" class="type">
+						<option value="Chien">Chien</option>
+						<option value="Chat">Chat</option>
+						<option value="Autre">Autre</option>
+					</select>
+					</br></br>
+					
+					<select name="Probleme" class="probleme">
+						<option value="Consultation">Consultation</option> 
+						<option value="Vaccination">Vaccination</option>
+						<option value="Sterilisation">Sterilisation</option>    
+						<option value="Chirurgie">Chirurgie</option>
+						<option value="Hospitalisation">Hospitalisation</option>
+						<option value="Autre">Autre</option>
+					</select>
+					</br></br>
+
+
+					<textarea name="AVIS" rows="5" cols="50"></textarea>
+					<button type="button" class="btn btn-default EnvoieForm">ENVOYER</button>
+			</form>
+		</div>
+
 		<div class="localisation">
 			<h1>Localisation</h1>
 			<p class="adress_cont">
@@ -17,38 +55,6 @@
 			</p>
 			<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9537895510025!2d-2.0795690492333927!3d47.432344407890994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480f80abaa6c1de3%3A0x7f75613c939f0679!2sClinique+V%C3%A9t%C3%A9rinaire+du+Docteur+Wintz!5e0!3m2!1sfr!2sfr!4v1472828074008" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 		</div>
-		
-		<form action="formulaire de contact" method="post">
-				
-				<input type="text" name="nom" placeholder="NOM" class="identite"></input> 
-				</br></br>
-				
-				<input type="text" name="Prenom" placeholder="Prenom"></input>
-				</br></br>
-				
-				<input type="text" name="Animal" placeholder="Nom de l'animal"></input>
-				</br></br>
+	</div>
 
-
-				<select name="Type" class="type">
-		       		<option value="Chien">Chien</option>
-		        	<option value="Chat">Chat</option>
-		        	<option value="Autre">Autre</option>
-		         </select>
-		         </br></br>
-		         
-		        <select name="Probleme" class="probleme">
-		        	<option value="Consultation">Consultation</option> 
-		        	<option value="Vaccination">Vaccination</option>
-		        	<option value="Sterilisation">Sterilisation</option>    
-		        	<option value="Chirurgie">Chirurgie</option>
-		        	<option value="Hospitalisation">Hospitalisation</option>
-		        	<option value="Autre">Autre</option>
-		        </select>
-		        </br></br>
-
-		        <textarea name="AVIS"></textarea>
-
-		        <button type="button" class="btn btn-default EnvoieForm">ENVOYER</button>
-		</form>
 	<?php get_footer(); ?>
