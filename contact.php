78abc912f91191b14f4f5227403ee6a4213b6a3b
FloryanL <floryan.lollivier@gmail.com>
2016-09-07 12:24:28 +0200

    modifs formulaire margin

--- contact.php
+++ contact.php
@@ -23,7 +23,7 @@
 					<input type="text" name="Animal" placeholder="Nom de l'animal"></input>
 					</br></br>
 
-					<p>Type d'animal :</p>
+					<label id="typan">Type d'animal :</label>
 					<select name="Type" class="type">
 						<option value="Chien">Chien</option>
 						<option value="Chat">Chat</option>
@@ -41,16 +41,17 @@
 					</select>
 					</br></br>
 
-					<textarea name="AVIS" rows="5" cols="50"></textarea>
+					<textarea name="AVIS" rows="10" cols="50"></textarea>
+					</br></br>
 					<button type="button" class="btn btn-default EnvoieForm">ENVOYER</button>
 			</form>
 		</div>
 
 		<div class="localisation">
 			<p id="locapara">
-			32 rue du vélodrome à Pontchâteau (Loire-Atlantique)
+			32 rue du vélodrome à Pontchâteau (Loire-Atlantique).
 			Du lundi au vendredi de 9h à 12h et de 14h à 19h et le samedi de 9h à 12h.
-			02 40 01 61 69
+			02 40 01 61 69.
 			</p>
 			<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9537895510025!2d-2.0795690492333927!3d47.432344407890994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480f80abaa6c1de3%3A0x7f75613c939f0679!2sClinique+V%C3%A9t%C3%A9rinaire+du+Docteur+Wintz!5e0!3m2!1sfr!2sfr!4v1472828074008" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 		</div>
