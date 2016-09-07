d04a4e704dab432af6d33232c80d96085222a2e2
jipee37 <jipee37@yahoo.fr>
2016-09-07 13:57:35 +0200

    ajout

--- contact.php
+++ contact.php
@@ -8,15 +8,13 @@
 
 <!--Formulaire de contact-->
 	<h2 class="TitreForm">NOUS CONTACTER PAR MAIL</h2>
-		<div>
-    <h1>Localisation</h1>
-    <p>
-    32 rue du vélodrome à Pontchâteau (Loire-Atlantique)
-    Du lundi au vendredi de 9h à 12h et de 14h à 19h et le samedi de 9h à 12h.
-    02 40 01 61 69
-    </p>
-    <iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9537895510025!2d-2.0795690492333927!3d47.432344407890994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480f80abaa6c1de3%3A0x7f75613c939f0679!2sClinique+V%C3%A9t%C3%A9rinaire+du+Docteur+Wintz!5e0!3m2!1sfr!2sfr!4v1472828074008" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
-</div>
+
+    
+    
+<div>
+	
+	<div>
+
 		<form action="formulaire de contact" method="post">
 				
 				<input type="text" name="nom" placeholder="NOM" class="identite"></input> 
@@ -46,8 +44,26 @@
 		        </select>
 		        </br></br>
 
-		        <textarea name="AVIS"></textarea>
+		        <textarea rows="15"  cols="50"name="AVIS"></textarea>
 
 		        <button type="button" class="btn btn-default EnvoieForm">ENVOYER</button>
-		</form>
+		<form>
+	</div>
+
+	<h1>Localisation</h1>
+
+	<div>
+		
+    	<p>
+    	32 rue du vélodrome à Pontchâteau (Loire-Atlantique)
+    	Du lundi au vendredi de 9h à 12h et de 14h à 19h et le samedi de 9h à 12h.
+    	02 40 01 61 69
+    	</p>
+
+		<iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2698.9537895510025!2d-2.0795690492333927!3d47.432344407890994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480f80abaa6c1de3%3A0x7f75613c939f0679!2sClinique+V%C3%A9t%C3%A9rinaire+du+Docteur+Wintz!5e0!3m2!1sfr!2sfr!4v1472828074008" width="400" height="400" frameborder="0" allowfullscreen="allowfullscreen">
+		</iframe>
+	</div>		
+
+</div>
+	
 	<?php get_footer(); ?>
