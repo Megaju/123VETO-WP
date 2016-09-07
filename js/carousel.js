7720cbca13d4e53aef5b6a1ee8acec3a86597395
solenehuault <solene.huault@gmail.com>
2016-09-07 14:13:55 +0200

    début de la mise en place du carousel

---
+++ js/carousel.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#carousel2").click(function() {
+		$(".carousel-img").css("background-image", "url(img/dr-aurelie.jpg)");
+		$("#carousel-titre").html("Text1");
+		$("#carousel-text").html("Blablablabla");
+	});
+});
