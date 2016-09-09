0a8e5b63e87efec2ae45fc0bd6a4a261a1d7d040
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-09 14:31:24 +0200

    accordeon espace vente en cours

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,5 +1,5 @@
+//FICHE PRESTATION
 function affiche_presta(target) {
-<<<<<<< HEAD
     document.getElementById("consultation").style.display = "none";
     document.getElementById("imagerie").style.display = "none";
     document.getElementById("analyse").style.display = "none";
@@ -7,6 +7,19 @@ function affiche_presta(target) {
     document.getElementById("hospitalisation").style.display = "none";
     document.getElementById("nac").style.display = "none";
     document.getElementById(target).style.display = "block";
+};
+
+//FICHE CONSEILS
+function fiche_conseil(target) {
+	document.getElementById("dents").style.display="none";
+	document.getElementById("yeux").style.display="none";
+	document.getElementById("pelage").style.display="none";
+	document.getElementById("parasites").style.display="none";
+	document.getElementById("vaccinChien").style.display="none";
+	document.getElementById("vaccinChat").style.display="none";
+	document.getElementById("identification").style.display="none";
+	document.getElementById("education").style.display="none";
+	document.getElementById(target).style.display="block";
 };
 
 //VENTE
@@ -19,25 +32,3 @@ $( document ).ready(function() {
         }
     }); 
 });
-=======
-	document.getElementById("consultation").style.display="none";
-	document.getElementById("imagerie").style.display="none";
-	document.getElementById("analyse").style.display="none";
-	document.getElementById("chirurgie").style.display="none";
-	document.getElementById("hospitalisation").style.display="none";
-	document.getElementById("nac").style.display="none";
-	document.getElementById(target).style.display="block";
-};
-
-function fiche_conseil(target) {
-	document.getElementById("dents").style.display="none";
-	document.getElementById("yeux").style.display="none";
-	document.getElementById("pelage").style.display="none";
-	document.getElementById("parasites").style.display="none";
-	document.getElementById("vaccinChien").style.display="none";
-	document.getElementById("vaccinChat").style.display="none";
-	document.getElementById("identification").style.display="none";
-	document.getElementById("education").style.display="none";
-	document.getElementById(target).style.display="block";
-};
->>>>>>> 8f86edfab76c45f7872d31cec93d9326c3c37a13
