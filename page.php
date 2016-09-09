28c186ca76826521ec418f420df982dbef27dba6
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-09 16:25:49 +0200

    changement pour les intros

--- page.php
+++ page.php
@@ -6,7 +6,7 @@ Template Name: prestations
 */
 ?>
 
-       <!-- pour insérer le header -->
+        <!-- pour insérer le header -->
         <?php get_header(); ?>
 
             <div id="attente-bootstrap" class="placement">
@@ -15,9 +15,26 @@ Template Name: prestations
 
                     <div>
                         <section class="formeindex">
-                            <h2>Notre savoir-faire</h2>
-                            <div class="index-separator"></div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas aspernatur dolorum ullam quisquam ut qui velit molestias inventore esse asperiores modi possimus.</p>
+                            
+                            <div class="uniform-article">
+                                <?php query_posts('category_name=titre-intro-prestation'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
+
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
+                            </div>
+                            
                             <div class="icone-generique">
                                 <div class="center-generique">
                                     <i class="fa fa-stethoscope fa-3x"></i>
@@ -50,133 +67,150 @@ Template Name: prestations
                                 </div>
                             </div>
                         </section>
-                        
+
                         <section>
-                           
+
                             <div class="uniform-article" id="consultation">
-                           <?php query_posts('category_name=consultation'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
+                                <?php query_posts('category_name=consultation'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
-                            </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
-                   
-                   <div class="uniform-article" id="imagerie">
-                            <?php query_posts('category_name=imagerie'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
                             </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
-                   
-                   <div class="uniform-article" id="analyse">
-                            <?php query_posts('category_name=analyses'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
-                            </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
-                   
-                   <div class="uniform-article" id="chirurgie">
-                            <?php query_posts('category_name=chirurgie'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
+                            <div class="uniform-article" id="imagerie">
+                                <?php query_posts('category_name=imagerie'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
-                            </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
-                   
-                   <div class="uniform-article" id="hospitalisation">
-                            <?php query_posts('category_name=hospitalisation'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
                             </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
-                   
-                   <div class="uniform-article" id="nac">
-                            <?php query_posts('category_name=nac'); ?>
-                            <?php if(have_posts()) : ?>
-                            <?php while(have_posts()) : the_post();?>
 
-                            <!-- affichage des articles -->
-                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>	
-                            <div class="index-separator"></div>
-                            <div class= "post" id="post-<?php the_ID(); ?>"> 
-                                <div class= "post_content alignement_article">
-                                    <div class="imgPage"> <?php the_post_thumbnail(array(250,250));?>
-                                    </div>
-                                   
-                                   <div class="contenu_article"><?php the_content(); ?></div>
-                                </div> 
+                            <div class="uniform-article" id="analyse">
+                                <?php query_posts('category_name=analyses'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
+
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
+
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
                             </div>
-                            <?php endwhile; ?>
-                            <?php endif; ?>
-                   </div>
+
+                            <div class="uniform-article" id="chirurgie">
+                                <?php query_posts('category_name=chirurgie'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
+
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
+
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
+                            </div>
+
+                            <div class="uniform-article" id="hospitalisation">
+                                <?php query_posts('category_name=hospitalisation'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
+
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
+
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
+                            </div>
+
+                            <div class="uniform-article" id="nac">
+                                <?php query_posts('category_name=nac'); ?>
+                                    <?php if(have_posts()) : ?>
+                                        <?php while(have_posts()) : the_post();?>
+
+                                            <!-- affichage des articles -->
+                                            <h2 title="<?php the_title(); ?>"><?php the_title(); ?></h2>
+                                            <div class="index-separator"></div>
+                                            <div class="post" id="post-<?php the_ID(); ?>">
+                                                <div class="post_content alignement_article">
+                                                    <div class="imgPage">
+                                                        <?php the_post_thumbnail(array(250,250));?>
+                                                    </div>
+
+                                                    <div class="contenu_article">
+                                                        <?php the_content(); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endwhile; ?>
+                                                <?php endif; ?>
+                            </div>
                         </section>
                     </div>
             </div>
-            
+
 
             <!-- pour insérer le footer -->
             <?php get_footer(); ?>
-
