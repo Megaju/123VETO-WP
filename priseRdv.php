7026a182962c51b2292493a5d1c6470d9f93e487
stephane-legalliard <le-galliard.stephane@orange.fr>
2016-09-09 16:39:26 +0200

    changement couleur

--- priseRdv.php
+++ priseRdv.php
@@ -9,61 +9,7 @@
 
 <section id="rdv">
 
-<<<<<<< HEAD
-      
-      <form action="formulaire de contact" method="post" id="form1">
-          
-          <input type="text" name="nom" placeholder="NOM" class="identite"></input> 
-          </br></br>
-          
-          <input type="text" name="Prenom" placeholder="Prenom"></input>
-          </br></br>
-          
-          <input type="text" name="Animal" placeholder="Nom de l'animal"></input>
-          </br></br>
 
-          <label id="typan">Vétérinaires :</label>
-          <select name="Type" class="type1">
-            <option value="veto1">Dr Aurélie Mayoussier</option>
-            <option value="veto2">Dr Eric Wintz</option>
-          </select>
-          </br></br>
-
-          <label id="typan">Type d'animal :</label>
-          <select name="Type" class="type1">
-            <option value="Chien">Chien</option>
-            <option value="Chat">Chat</option>
-            <option value="Autre">Autre</option>
-          </select>
-          </br></br>
-          
-          <select name="Probleme" class="type1">
-            <option value="Consultation">Consultation</option> 
-            <option value="Vaccination">Vaccination</option>
-            <option value="Sterilisation">Sterilisation</option>    
-            <option value="Chirurgie">Chirurgie</option>
-            <option value="Hospitalisation">Hospitalisation</option>
-            <option value="Autre">Autre</option>
-          </select>
-          </br></br>
-
-          <textarea name="AVIS"></textarea>
-          </br></br>
-          
-
-        <div id="bouton">
-          <input type="submit" value="Envoyer">
-          <input type="reset" value="Effacer">
-        </div>
-      </form>
-      </div>
-
-      <div class="agenda">
-
-        <legend>
-          <h1>Agenda</h1></legend>
-        <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;width=600 height=200&amp;wkst=2&amp;hl=fr&amp;bgcolor=%23FFFFFF&amp;src=ngnn1lug1h2vh1inja9180alqk%40group.calendar.google.com&amp;color=%23875509&amp;ctz=Europe%2FParis"
-=======
       <form action="mon-formulaire" method="post">
             <div class="section">
                         <h1>Rendez-vous</h1>
@@ -107,8 +53,7 @@
                <div class="agenda">      
                         <h1>Agenda</h1>
         <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=500&amp;wkst=2&amp;hl=fr&amp;bgcolor=%23FFFFFF&amp;src=ngnn1lug1h2vh1inja9180alqk%40group.calendar.google.com&amp;color=%23875509&amp;ctz=Europe%2FParis"
->>>>>>> e69c30c52beba56903566bfa279ba0fa57a62b77
-          frameborder="0" scrolling="no"></iframe>
+   frameborder="0" scrolling="no"></iframe>
                </div>
             </div> 
                   
