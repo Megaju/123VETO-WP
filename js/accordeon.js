5db10c8d8a539f14fb7a54cb03c0947edafa5dd2
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-09 14:22:50 +0200

    accordeon espace vente en cours

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,9 +1,20 @@
 function affiche_presta(target) {
-	document.getElementById("consultation").style.display="none";
-	document.getElementById("imagerie").style.display="none";
-	document.getElementById("analyse").style.display="none";
-	document.getElementById("chirurgie").style.display="none";
-	document.getElementById("hospitalisation").style.display="none";
-	document.getElementById("nac").style.display="none";
-	document.getElementById(target).style.display="block";
+    document.getElementById("consultation").style.display = "none";
+    document.getElementById("imagerie").style.display = "none";
+    document.getElementById("analyse").style.display = "none";
+    document.getElementById("chirurgie").style.display = "none";
+    document.getElementById("hospitalisation").style.display = "none";
+    document.getElementById("nac").style.display = "none";
+    document.getElementById(target).style.display = "block";
 };
+
+//VENTE
+$( document ).ready(function() {
+    $("#medicament").click(function() {
+        if ($("#article").css("display") == "none") {
+            $("#article").css("display", "block");
+        } else {
+            $("#article").css("display", "none");
+        }
+    }); 
+});
