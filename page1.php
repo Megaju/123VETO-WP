66404b3d550df204b6cebf552892a82912e639e1
Becca85 <rebeccamaccio@gmail.com>
2016-09-14 14:04:44 +0200

    remise encore a jour de Capa dans CSS et page conseil

--- page1.php
+++ page1.php
@@ -66,7 +66,7 @@ Template Name: conseils
                         
                         <div class="icone-generique">
                             <div class="center-generique">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 512 512" enable-background="new 0 0 512 512" width="60px" height="60px">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" viewBox="0 0 512 512" enable-background="new 0 0 512 512" width="60px" height="60px">
                                   <g>
                                       <path d="m495,93.5l-76.2-76.5c-8-8-20.9-8-28.9,0-8,8-8,20.9 0,28.9l23.7,23.8-19.9,19.9 28.8,28.9 19.9-19.9 23.7,23.7c12.5,11 24.9,4 28.9,0 8-7.9 8-20.9 0-28.8z" fill="#F58900"/>
                                       <path d="m320.8,74.2c-8-8-20.9-8-28.9,0-8,8-8,20.9 0,28.9l11,11-102.2,102.2 94.9,95.1 102.2-102.2 11,11c3.7,4.7 18,10.9 28.9,0 8-8 8-20.9 0-28.9l-116.9-117.1z" fill="#F58900"/>
@@ -93,7 +93,7 @@ Template Name: conseils
                             </div>
                             
                             <div class="center-generique">
-                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve" width="60px" height="60px">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 512.001 512.001" style="enable-background:new 0 0 512.001 512.001;" xml:space="preserve" width="60px" height="60px">
                                     <g>
                                         <circle cx="119.646" cy="117.515" r="40.848" fill="#F58900"/>
                                     </g>
