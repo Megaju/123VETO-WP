c180282eb97735f513a1acf1c2fdbb380a8de01f
Megaju <julien_malle@outlook.fr>
2016-09-15 17:20:47 +0200

    modif style newsletter part1

--- news.php
+++ news.php
@@ -8,14 +8,16 @@ Template Name: newsletter
         <h2><?php the_title(); ?></h2>
         
         <!-- AFFICHAGE DU POST -->
-        <section>
+        <section id="newsletter">
             <?php query_posts('category_name=news') ?>
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-                <?php the_post_thumbnail(array(250,250));?>
-                <p>
-                    <?php the_content(__('<button class="read-more">Lire la suite</button>')); ?>
-                </p>
+                <div class="actu-on-newsletter">    
+                    <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                    <?php the_post_thumbnail(array(250,250));?>
+                    <p>
+                        <?php the_content(__('<button class="read-more">Lire la suite</button>')); ?>
+                    </p>
+                </div>
             <?php endwhile; else: ?>
                 <p>
                     <?php _e('Désolé, il n\'a aucun article pour le moment.'); ?>
