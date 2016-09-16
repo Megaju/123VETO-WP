52bedb7b04531d477605fd0d6ee03e26d6e2fa05
Megaju <julien_malle@outlook.fr>
2016-09-16 14:51:18 +0200

    style page newletter ok

--- news.php
+++ news.php
@@ -5,7 +5,7 @@ Template Name: newsletter
 ?>
     <?php get_header() ?>
         <!-- TITRE DE LA PAGE -->
-        <h2><?php the_title(); ?></h2>
+        <h2 class="bandeau bandeau-mt0"><?php the_title(); ?></h2>
         
         <!-- AFFICHAGE DU POST -->
         <section id="newsletter">
@@ -13,10 +13,12 @@ Template Name: newsletter
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <div class="actu-on-newsletter">    
                     <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                    <div class="news-img">
                     <?php the_post_thumbnail(array(250,250));?>
-                    <p>
+                    </div>
+                    <div class="news-p">
                         <?php the_content(__('<button class="read-more">Lire la suite</button>')); ?>
-                    </p>
+                    </div>
                 </div>
             <?php endwhile; else: ?>
                 <p>
