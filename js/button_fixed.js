221face9ba050631a30acf34618fb24a1013c866
Megaju <julien_malle@outlook.fr>
2016-09-21 13:43:24 +0200

    modif condition boutton RDV js

--- js/button_fixed.js
+++ js/button_fixed.js
@@ -2,24 +2,39 @@ $(document).ready(function () {
 	var origOffsetY = $('#priseRDV').offset().top;
     
     var winwidth = $(window).width();
-
-	function scroll() {
-		if ($(window).scrollTop() >= origOffsetY) {
-			$('#priseRDV').css("position", "fixed");
-			$('#priseRDV').css("top", "0px");
-			$('#priseRDV').css("margin-top", "0");
-            $('.content').css("margin-top", "0");
-		} else if (winwidth <= 1085) {
-			$('#priseRDV').css("position", "absolute");
-			$('#priseRDV').css("top", "auto");
-			$('#priseRDV').css("margin-top", "0px");
-            $('.content').css("margin-top", "60px");
-		} else {
-			$('#priseRDV').css("position", "absolute");
-			$('#priseRDV').css("top", "auto");
-			$('#priseRDV').css("margin-top", "0px");
-            $('.content').css("margin-top", "0");
-		}
-	}
+    
+    if (winwidth <= 1085) {
+        function scroll() {
+            if ($(window).scrollTop() >= origOffsetY) {
+                $('#priseRDV').css("position", "fixed");
+                $('#priseRDV').css("top", "0px");
+                $('#priseRDV').css("margin-top", "0");
+                $('.content').css("margin-top", "0");
+            } 
+            else {
+                $('#priseRDV').css("position", "absolute");
+                $('#priseRDV').css("top", "auto");
+                $('#priseRDV').css("margin-top", "0px");
+                $('.content').css("margin-top", "60px");
+            }    
+    } 
+    else {
+        function scroll() {
+            if ($(window).scrollTop() >= origOffsetY) {
+                $('#priseRDV').css("position", "fixed");
+                $('#priseRDV').css("top", "0px");
+                $('#priseRDV').css("margin-top", "0");
+                $('.content').css("margin-top", "0");
+            } 
+            else {
+                $('#priseRDV').css("position", "absolute");
+                $('#priseRDV').css("top", "auto");
+                $('#priseRDV').css("margin-top", "0px");
+                $('.content').css("margin-top", "0");
+            }
+	
+        }
+    
+	
 	document.onscroll = scroll;
 });
