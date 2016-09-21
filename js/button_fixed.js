7953d1373bd3768c2d7d884c4ac706367db341b6
Megaju <julien_malle@outlook.fr>
2016-09-21 14:41:36 +0200

    bouton RDV mieux, mais reste des bugs

--- js/button_fixed.js
+++ js/button_fixed.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
     
     var winwidth = $(window).width();
     
-    if (winwidth <= 1085) {
+    
         function scroll() {
-            if ($(window).scrollTop() >= origOffsetY) {
+            if (winwidth <= 1085) {
+                if ($(window).scrollTop() >= origOffsetY) {
                 $('#priseRDV').css("position", "fixed");
                 $('#priseRDV').css("top", "0px");
                 $('#priseRDV').css("margin-top", "0");
@@ -16,23 +17,8 @@ $(document).ready(function () {
                 $('#priseRDV').css("top", "auto");
                 $('#priseRDV').css("margin-top", "0px");
                 $('.content').css("margin-top", "60px");
-            }    
-    } 
-    else {
-        function scroll() {
-            if ($(window).scrollTop() >= origOffsetY) {
-                $('#priseRDV').css("position", "fixed");
-                $('#priseRDV').css("top", "0px");
-                $('#priseRDV').css("margin-top", "0");
-                $('.content').css("margin-top", "0");
-            } 
-            else {
-                $('#priseRDV').css("position", "absolute");
-                $('#priseRDV').css("top", "auto");
-                $('#priseRDV').css("margin-top", "0px");
-                $('.content').css("margin-top", "0");
             }
-	
+            }    
         }
     
 	
